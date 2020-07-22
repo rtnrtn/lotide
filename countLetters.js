26b2177f2e14680399e033d1b76e1647bc9e77c3
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-07-22 20:19:19 +0000

    Function that takes in a string and returns a count of each letter as an object

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let results = {};
+  let stringNoSpaces = string.split(" ").join("");
+  for (let char of stringNoSpaces) {
+    if (results[char] === undefined) {
+      results[char] = 1;
+    } else {
+      results[char] += 1;
+    }
+  }
+  return results;
+};
+
+const string1 = countLetters("This function counts letters not spaces.");
+
+assertEqual(string1["T"], 1);
+assertEqual(string1["s"], 5);
+assertEqual(string1["n"], 4);
