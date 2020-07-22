9e0ad91ce1066c577816cf623ef070d90626cfdb
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-07-22 01:33:49 +0000

    Create function that takes a source array and a second array of items to remove, returning a new array without items in the second array

---
+++ without.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB)) {
+    console.log(`✅✅✅ Assertion Passed: [${JSON.stringify(arrayA)}] === [${JSON.stringify(arrayB)}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${JSON.stringify(arrayA)}] !== [${JSON.stringify(arrayB)}]`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let arrayWithoutItems = [];
+  for (let i = 0; i < source.length; i++) {
+    let itemShouldBeAddedToArray = true;
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        itemShouldBeAddedToArray = false;
+        break;
+      }
+    }
+    if (itemShouldBeAddedToArray) {
+      arrayWithoutItems.push(source[i]);
+    }
+  }
+  return arrayWithoutItems;
+};
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without([4, 5, 6],[1, 2, 3, 4, 5, 6]));
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
