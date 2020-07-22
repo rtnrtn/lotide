fbf0d1740496b46e43cb2ee117c1b51ce6a0b25d
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-07-22 23:53:24 +0000

    Function that searches for matching key within object given a specified value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(bestTVShows, nameOfTVShow) {
+  for (let i of Object.keys(bestTVShows)) {
+    if (bestTVShows[i] === nameOfTVShow) {
+      return i;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
