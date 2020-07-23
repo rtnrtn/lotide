8f62bfb7358d04560705e1e7661eeb11e0072e0b
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-07-23 01:45:16 +0000

    Function that asserts whether two objects are equal

---
+++ assertObjectsEqual.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(objectA, objectB) {
+  if (Object.values(objectA).length !== Object.values(objectB).length) {
+    return false;
+  }
+  for (let i of Object.keys(objectA)) {
+    if (Array.isArray(objectA[i]) && Array.isArray(objectB[i])) {
+      return eqArrays(objectA[i], objectB[i]);
+    }
+    if (objectA[i] !== objectB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+assertObjectsEqual({ a: "1", b: "2" }, { b: "2", a: "1" });
+assertObjectsEqual({ a: "1", b: "2" }, { a: "1", b: "2", c: "3" });
+assertObjectsEqual({ c: "1", d: ["2", 3] }, { d: ["2", 3], c: "1" });
+assertObjectsEqual({ c: "1", d: ["2", 3] }, { c: "1", d: ["2", 3, 4] });
