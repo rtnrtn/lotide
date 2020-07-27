3595e2a340749faeea91a9c218c2049f4dee4dec
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-07-27 17:34:59 +0000

    Refactoring using modules to share code between files

--- head.js
+++ head.js
@@ -1,18 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   let firstElement = array[0];
   return firstElement;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
